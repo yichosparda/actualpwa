c34b0615e75a9dc4d6de24a97be45b6c4fa4bcce
yichosparda <jasmine.yuan@education.nsw.gov.au>
2026-05-27 12:21:12 +1000

    database and images

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,3 +1,13 @@
 -- SQLite
 
-ALTER TABLE accounts RENAME COLUMN passsword TO password;
+-- CREATE TABLE items(
+--     product TEXT NOT NULL,
+--     variant TEXT DEFAULT 'N/A',
+--     stock INT CHECK(stock >= 0),
+--     price DECIMAL NOT NULL
+-- );
+
+INSERT INTO items(product,variant, stock, price)
+VALUES('cheesecake','N/A',20, 90.50),
+    ('cheesecake','gluten_free',10, 110.75),
+    ('cheesecake','dairy_free',10, 102)
